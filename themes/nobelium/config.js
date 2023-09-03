fb99ee2e0c7eaf5c99541727f32fa77cdf5198da
猿恋 <leewint@qq.com>
2023-09-03 21:53:43 +0800

    Update config.js

--- themes/nobelium/config.js
+++ themes/nobelium/config.js
@@ -11,7 +11,7 @@ const CONFIG = {
   MENU_TAG: false, // 显示标签
   MENU_ARCHIVE: true, // 显示归档
   MENU_SEARCH: true, // 显示搜索
-  MENU_RSS: true // 显示订阅
+  MENU_RSS: false // 显示订阅
 
 }
 export default CONFIG
